fec5922b95b03a0f47004b6cb451fc44ec782649
subramanyaraju1112 <subramanyaraju1112@gmail.com>
2025-12-03 16:55:33 +0530

    updates: theme provider wrapped into base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { ThemeProvider } from "./components/theme-provider";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <AppRoutes />
-      </ThemeProvider>
+      <AppRoutes />
     </>
   );
 }
--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,15 +1,18 @@
 import { Footer, Header } from "@/components/common";
+import { ThemeProvider } from "@/components/theme-provider";
 import { Outlet } from "react-router-dom";
 
 const BaseLayout: React.FC = () => {
   return (
-    <section className="max-w-7xl mx-auto px-4 min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1 pt-28">
-        <Outlet />
-      </main>
-      <Footer />
-    </section>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <section className="max-w-7xl mx-auto px-4 min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1 pt-28">
+          <Outlet />
+        </main>
+        <Footer />
+      </section>
+    </ThemeProvider>
   );
 };
 
